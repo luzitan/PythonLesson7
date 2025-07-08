90a27c10ff1daba879c7d2a5951e388e2dc7ef3b
Vera Bylinskaia <iagyar@list.ru>
2025-07-08 12:12:41 +0300

    Задача 02

--- task02.py
+++ task02.py
@@ -10,8 +10,15 @@ numbers: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
 Результат работы программы:
 [('a', 1), ('b', 2), ('c', 3), ('d', 4), ('e', 5)]
 """
+from typing import List, Tuple
+
 
 letters = ['a', 'b', 'c', 'd', 'e']
 numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 
-print([(letters[i], numbers[i]) for i in range(len(letters)) if i < len(numbers)])
+# # Вариант 1
+# print([(letters[i], numbers[i]) for i in range(len(letters)) if i < len(numbers)])
+
+# Вариант 2
+results: List[Tuple[str, int]] = list(map(lambda x, y: (x, y), letters, numbers))
+print(results)
